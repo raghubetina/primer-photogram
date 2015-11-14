ed1fdffd9dae3a48179a330ceb5e0229792560fe
Raghu Betina <rbetina@users.noreply.github.com>
2015-11-14 09:36:32 -0600

    Generate Photo resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114153543) do
+ActiveRecord::Schema.define(version: 20151114153620) do
+
+  create_table "photos", force: :cascade do |t|
+    t.text     "caption"
+    t.string   "image"
+    t.integer  "owner_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
