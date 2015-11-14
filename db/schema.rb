73d6b8161c655759c26e62d801ae7eaf4c0b5b33
Raghu Betina <rbetina@users.noreply.github.com>
2015-11-14 09:40:55 -0600

    Generate Comment resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114153620) do
+ActiveRecord::Schema.define(version: 20151114154029) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "photo_id"
+    t.text     "body"
+    t.integer  "commenter_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "photos", force: :cascade do |t|
     t.text     "caption"
