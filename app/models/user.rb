ea1e0c917597d8d8e5987642c2ad5deccbd4da31
Raghu Betina <rbetina@users.noreply.github.com>
2015-11-15 11:20:08 -0600

    Destroy friend requests when user is destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ActiveRecord::Base
   has_many :likes, :dependent => :destroy
   has_many :liked_photos, :through => :likes, :source => :photo
 
-  has_many :friend_requests_where_sender, :class_name => "FriendRequest", :foreign_key => "sender_id"
+  has_many :friend_requests_where_sender, :class_name => "FriendRequest", :foreign_key => "sender_id", :dependent => :destroy
   has_many :friends_where_sender, :through => :friend_requests_where_sender, :source => :receiver
 
-  has_many :friend_requests_where_receiver, :class_name => "FriendRequest", :foreign_key => "receiver_id"
+  has_many :friend_requests_where_receiver, :class_name => "FriendRequest", :foreign_key => "receiver_id", :dependent => :destroy
   has_many :friends_where_receiver, :through => :friend_requests_where_receiver, :source => :sender
 
   has_many :timeline_photos, :through => :friends_where_sender, :source => :photos
