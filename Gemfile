fb729d131497280c85977a62a8823b119e0b6d93
Raghu Betina <raghu@firstdraft.com>
2017-05-02 12:19:42 -0500

    Dumped database to seeds

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'carrierwave'
 gem 'fog'
 gem 'mini_magick'
 
+gem 'seed_dump'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,130 +1,105 @@
-User.destroy_all
-Photo.destroy_all
-Comment.destroy_all
-Like.destroy_all
-FriendRequest.destroy_all
-
-usernames = ["alice", "bob", "carol", "dave", "ellen"]
-
-usernames.each do |username|
-  user = User.create
-  user.username = username
-  user.email = "#{username}@example.com"
-  user.password = "12341234"
-  user.save
-end
-
-puts "There are now #{User.count} users in the database."
-
-photo_info = [
-  {
-    :image => "astronaut.jpg",
-    :caption => "Astronaut"
-  },
-  {
-    :image => "aurora.jpg",
-    :caption => "Aurora"
-  },
-  {
-    :image => "birds.jpg",
-    :caption => "Birds"
-  },
-  {
-    :image => "bridge.jpg",
-    :caption => "Bridge"
-  },
-  {
-    :image => "firebreather.jpg",
-    :caption => "Firebreather"
-  },
-  {
-    :image => "horses.jpg",
-    :caption => "Horses"
-  },
-  {
-    :image => "lake.jpg",
-    :caption => "Lake"
-  },
-  {
-    :image => "lightbulb.jpg",
-    :caption => "Lightbulb"
-  },
-  {
-    :image => "penguins.jpg",
-    :caption => "Penguins"
-  },
-  {
-    :image => "pluto.jpg",
-    :caption => "Pluto"
-  },
-  {
-    :image => "squirrel.jpg",
-    :caption => "Squirel"
-  },
-  {
-    :image => "stained_glass.jpg",
-    :caption => "Stained glass"
-  },
-  {
-    :image => "sun.jpg",
-    :caption => "Sun"
-  },
-  {
-    :image => "telescope.jpg",
-    :caption => "Telescope"
-  },
-  {
-    :image => "tower.jpg",
-    :caption => "Tower"
-  },
-  {
-    :image => "turtles.jpg",
-    :caption => "Turtles"
-  }
-]
-
-users = User.all
-
-photo_info.each do |photo_hash|
-  filename = photo_hash[:image]
-
-  photo = Photo.new
-  photo.image = File.open(Rails.root.join('lib', 'assets', filename).to_s)
-  photo.caption = photo_hash[:caption]
-  photo.user = users.sample
-  photo.created_at = rand(2.years.ago..Time.now)
-  photo.save
-end
-
-puts "There are now #{Photo.count} photos in the database."
-
-photos = Photo.all
-
-photos.each do |photo|
-  rand(6).times do
-    comment = photo.comments.build
-    comment.user = users.sample
-    comment.body = Faker::Hacker.say_something_smart
-    comment.save
-  end
-end
-
-puts "There are now #{Comment.count} comments in the database."
-
-photos.each do |photo|
-  users.sample(rand(users.count)).each do |user|
-    like = photo.likes.build
-    like.user = user
-    like.save
-  end
-end
-
-puts "There are now #{Like.count} likes in the database."
-
-users.each do |receiver|
-  users.sample(rand(users.count)).each do |sender|
-    FriendRequest.create sender: sender, receiver: receiver
-  end
-end
-
-puts "There are now #{FriendRequest.count} friend requests in the database."
+User.create!([
+  {email: "alice@example.com", encrypted_password: "$2a$10$lm9FMeGbn8rBbEHB7JX8d.jIYdLrf2NQNARsw2zD1bQFoBOeRPwYq", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 4, current_sign_in_at: "2017-03-28 13:13:13", last_sign_in_at: "2017-01-31 23:04:11", current_sign_in_ip: "::1", last_sign_in_ip: "::1", username: "alice"},
+  {email: "bob@example.com", encrypted_password: "$2a$10$2CcIse472fTfkaLFjxiCBebhGAk8fPqG.ZdnCS4xjwY5nPr6O2/yG", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, username: "bob"},
+  {email: "carol@example.com", encrypted_password: "$2a$10$CrV9M.d3ZXcXtPKqt4UR4ef7BoMsuWVNVSV3GXrwCDV4.lN9BIFP.", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, username: "carol"},
+  {email: "dave@example.com", encrypted_password: "$2a$10$Yk5pdCMmDdsdBoU.FRF8GeEN0LZs5S5VG30csTt4DoHJ8qH.OurEC", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2017-01-04 18:50:41", last_sign_in_at: "2017-01-04 18:50:41", current_sign_in_ip: "::1", last_sign_in_ip: "::1", username: "dave"},
+  {email: "ellen@example.com", encrypted_password: "$2a$10$fnudPElEmdAqsa6xWrY/luDx1Y/dVgY3uVTDAtUlepaPrqWxTz83W", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, username: "ellen"}
+])
+Comment.create!([
+  {photo_id: 224, body: "We need to hack the auxiliary RSS program!", user_id: 43},
+  {photo_id: 224, body: "Try to parse the SAS circuit, maybe it will input the digital monitor!", user_id: 45},
+  {photo_id: 224, body: "I'll override the cross-platform USB circuit, that should program the IB alarm!", user_id: 42},
+  {photo_id: 226, body: "Use the neural AI array, then you can transmit the virtual circuit!", user_id: 42},
+  {photo_id: 226, body: "Synthesizing the microchip won't do anything, we need to reboot the auxiliary sas application!", user_id: 45},
+  {photo_id: 226, body: "We need to index the auxiliary AGP bus!", user_id: 43},
+  {photo_id: 226, body: "Use the neural TCP feed, then you can input the virtual protocol!", user_id: 45},
+  {photo_id: 235, body: "Use the back-end USB firewall, then you can calculate the 1080p pixel!", user_id: 41},
+  {photo_id: 222, body: "I'll hack the cross-platform SMS port, that should port the SAS hard drive!", user_id: 42},
+  {photo_id: 222, body: "We need to connect the 1080p COM bus!", user_id: 45},
+  {photo_id: 222, body: "You can't generate the program without navigating the redundant PCI matrix!", user_id: 41},
+  {photo_id: 222, body: "Try to transmit the ADP system, maybe it will parse the auxiliary port!", user_id: 45},
+  {photo_id: 222, body: "We need to synthesize the cross-platform SSL bus!", user_id: 42},
+  {photo_id: 231, body: "Try to connect the FTP port, maybe it will transmit the auxiliary protocol!", user_id: 45},
+  {photo_id: 231, body: "Try to connect the PNG transmitter, maybe it will quantify the solid state firewall!", user_id: 41},
+  {photo_id: 231, body: "We need to program the solid state JSON pixel!", user_id: 42},
+  {photo_id: 233, body: "You can't input the alarm without copying the virtual AI port!", user_id: 43},
+  {photo_id: 233, body: "We need to copy the auxiliary JSON application!", user_id: 43},
+  {photo_id: 233, body: "The CSS capacitor is down, connect the wireless driver so we can back up the SCSI driver!", user_id: 42},
+  {photo_id: 233, body: "The SSL program is down, connect the cross-platform protocol so we can copy the SMS protocol!", user_id: 44},
+  {photo_id: 228, body: "The ADP system is down, navigate the redundant application so we can connect the HDD capacitor!", user_id: 45},
+  {photo_id: 228, body: "Programming the monitor won't do anything, we need to navigate the wireless ib monitor!", user_id: 43},
+  {photo_id: 228, body: "If we copy the matrix, we can get to the ADP circuit through the digital SMS circuit!", user_id: 41},
+  {photo_id: 228, body: "I'll back up the multi-byte SMTP feed, that should driver the COM feed!", user_id: 45},
+  {photo_id: 227, body: "If we compress the feed, we can get to the SMTP protocol through the digital FTP alarm!", user_id: 43},
+  {photo_id: 227, body: "The AGP firewall is down, connect the auxiliary feed so we can calculate the TCP circuit!", user_id: 42},
+  {photo_id: 227, body: "We need to copy the redundant SSL monitor!", user_id: 41},
+  {photo_id: 227, body: "The USB program is down, bypass the optical alarm so we can synthesize the PCI monitor!", user_id: 44},
+  {photo_id: 225, body: "Compressing the driver won't do anything, we need to synthesize the primary jbod program!", user_id: 41},
+  {photo_id: 225, body: "You can't back up the firewall without connecting the bluetooth ADP circuit!", user_id: 41},
+  {photo_id: 225, body: "If we quantify the bandwidth, we can get to the JBOD port through the multi-byte TCP hard drive!", user_id: 43},
+  {photo_id: 229, body: "Try to hack the SMS program, maybe it will input the multi-byte sensor!", user_id: 41},
+  {photo_id: 229, body: "You can't navigate the program without calculating the redundant IB bus!", user_id: 43},
+  {photo_id: 230, body: "You can't override the port without calculating the bluetooth COM bandwidth!", user_id: 45},
+  {photo_id: 230, body: "I'll parse the primary COM bus, that should hard drive the AGP array!", user_id: 41},
+  {photo_id: 223, body: "Bypassing the panel won't do anything, we need to parse the optical sdd hard drive!", user_id: 45},
+  {photo_id: 232, body: "The ADP feed is down, calculate the neural capacitor so we can copy the XSS feed!", user_id: 41},
+  {photo_id: 232, body: "Try to input the FTP panel, maybe it will quantify the multi-byte panel!", user_id: 42},
+  {photo_id: 232, body: "I'll navigate the neural CSS microchip, that should pixel the CSS card!", user_id: 42}
+])
+FriendRequest.create!([
+  {sender_id: 42, receiver_id: 41},
+  {sender_id: 44, receiver_id: 41},
+  {sender_id: 43, receiver_id: 41},
+  {sender_id: 44, receiver_id: 42},
+  {sender_id: 43, receiver_id: 42},
+  {sender_id: 45, receiver_id: 43},
+  {sender_id: 45, receiver_id: 44},
+  {sender_id: 42, receiver_id: 44},
+  {sender_id: 43, receiver_id: 44},
+  {sender_id: 41, receiver_id: 45},
+  {sender_id: 44, receiver_id: 45},
+  {sender_id: 41, receiver_id: 43}
+])
+Like.create!([
+  {user_id: 45, photo_id: 222},
+  {user_id: 44, photo_id: 231},
+  {user_id: 42, photo_id: 231},
+  {user_id: 41, photo_id: 236},
+  {user_id: 44, photo_id: 236},
+  {user_id: 43, photo_id: 236},
+  {user_id: 45, photo_id: 233},
+  {user_id: 44, photo_id: 233},
+  {user_id: 43, photo_id: 234},
+  {user_id: 45, photo_id: 228},
+  {user_id: 44, photo_id: 228},
+  {user_id: 42, photo_id: 228},
+  {user_id: 45, photo_id: 227},
+  {user_id: 41, photo_id: 225},
+  {user_id: 43, photo_id: 225},
+  {user_id: 44, photo_id: 225},
+  {user_id: 42, photo_id: 225},
+  {user_id: 43, photo_id: 230},
+  {user_id: 41, photo_id: 230},
+  {user_id: 44, photo_id: 230},
+  {user_id: 41, photo_id: 223},
+  {user_id: 44, photo_id: 232},
+  {user_id: 41, photo_id: 235}
+])
+Photo.create!([
+  {caption: "Astronaut", image: "astronaut.jpg", user_id: 44},
+  {caption: "Aurora", image: "aurora.jpg", user_id: 41},
+  {caption: "Birds", image: "birds.jpg", user_id: 44},
+  {caption: "Bridge", image: "bridge.jpg", user_id: 42},
+  {caption: "Firebreather", image: "firebreather.jpg", user_id: 43},
+  {caption: "Horses", image: "horses.jpg", user_id: 45},
+  {caption: "Lake", image: "lake.jpg", user_id: 44},
+  {caption: "Lightbulb", image: "lightbulb.jpg", user_id: 42},
+  {caption: "Penguins", image: "penguins.jpg", user_id: 44},
+  {caption: "Pluto", image: "pluto.jpg", user_id: 42},
+  {caption: "Squirel", image: "squirrel.jpg", user_id: 41},
+  {caption: "Stained glass", image: "stained_glass.jpg", user_id: 42},
+  {caption: "Sun", image: "sun.jpg", user_id: 43},
+  {caption: "Telescope", image: "telescope.jpg", user_id: 45},
+  {caption: "Tower", image: "tower.jpg", user_id: 42},
+  {caption: "Turtles", image: "turtles.jpg", user_id: 45}
+])
